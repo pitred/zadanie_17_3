176e0d0f5c02340e0013ebb195451e3a6e06fc67
Piotr Nowinski <pitred007git@gmail.com>
2019-05-04 15:01:07 +0200

    Add program.js

---
+++ program.js
@@ -0,0 +1,20 @@
+process.stdin.setEncoding("utf-8");
+process.stdin.on('readable', function () {
+    var input = process.stdin.read();
+    if (input !== null) {
+        var instruction = input.toString().trim();
+
+        switch (instruction) {
+            case 'exit':
+                process.stdout.write('Quitting app!\n');
+                process.exit();
+                break;
+            case 'info':
+                console.log('Node version: ' + process.versions.node);
+                process.stdout.write('Langue version: ' + process.env.LANG + '\n');
+                break;
+            default:
+                process.stdout.write('Wrong instruction!\n');
+        }
+    }
+});
